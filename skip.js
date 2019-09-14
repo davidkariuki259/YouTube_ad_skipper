a92a3978c3989b0f2ead7231bb64145614105fee
davidkariuki259 <51744197+davidkariuki259@users.noreply.github.com>
2019-09-14 09:57:56 +0300

    Update skip.js

--- skip.js
+++ skip.js
@@ -1,6 +1,6 @@
 setInterval(function click_ad_to_remove(){
-	var ad_button=document.getElementsByClassName('ytp-ad-skip-button ytp-button');
-	if (ad_button.length>0){
+	var ad_button=document.getElementsByClassName('ytp-ad-skip-button ytp-button');	//access the 'skip ad' button's parent class
+	if (ad_button.length>0){	//clicck button if class is not empty
 		document.getElementsByClassName('ytp-ad-skip-button ytp-button')[0].click();
 	}
 }, 3000);
